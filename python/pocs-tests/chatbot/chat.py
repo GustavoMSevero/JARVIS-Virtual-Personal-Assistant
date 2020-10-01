4c1c3f56f90f21ae08865bb778f377786c6f2d1a
yashgorey <65535268+yashgorey@users.noreply.github.com>
2020-10-01 19:38:22 +0530

    New_feature

--- python/pocs-tests/chatbot/chat.py
+++ python/pocs-tests/chatbot/chat.py
@@ -37,3 +37,66 @@ while True:
     question = input("You ")
     answer = bot.get_response(question)
     print("Assistant ", answer)
+    
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!") 
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=3)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+            speak('okay')
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+            speak('okay')
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+            speak('okay')
+
+        elif 'open facebook' in query:
+            webbrowser.open("facebook.com")
+            speak('okay')
